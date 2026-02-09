508d931044ff0a3a922976504099e701a84d5c4c
Scott Dacre-Curtis <scott.curtis@xero.com>
2026-02-10 09:06:53 +1300

    fix: update image source to use BASE_URL for logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,96 @@
-import { Provider } from 'jotai';
+import { Provider } from "jotai";
 import {
-    ApplicationSelector,
-    CommonQueriesPanelSection,
-    EnvironmentSelector,
-    FacetSelector,
-    Flex,
-    FlexItem,
-    QueryOptions,
-    MetricQueryBuilder,
-    QueryPreview,
-    TimePeriodSelector,
-} from './components';
+  ApplicationSelector,
+  CommonQueriesPanelSection,
+  EnvironmentSelector,
+  FacetSelector,
+  Flex,
+  FlexItem,
+  QueryOptions,
+  MetricQueryBuilder,
+  QueryPreview,
+  TimePeriodSelector,
+} from "./components";
 import XUIPanel, {
-    XUIPanelHeader,
-    XUIPanelHeading,
-    XUIPanelSection,
-    XUIPanelSectionHeading
+  XUIPanelHeader,
+  XUIPanelHeading,
+  XUIPanelSection,
+  XUIPanelSectionHeading,
 } from "@xero/xui/react/panel";
 
 function App() {
+  const header = (
+    <XUIPanelHeader>
+      <Flex align="center" gap="1rem">
+        <img
+          src={`${import.meta.env.BASE_URL}nr-builder-logo.svg`}
+          alt="NR Query Builder Logo"
+          style={{ height: "48px", width: "48px" }}
+        />
+        <XUIPanelHeading headingLevel={1}>
+          New Relic Query Builder
+        </XUIPanelHeading>
+      </Flex>
+    </XUIPanelHeader>
+  );
 
-    const header = (
-        <XUIPanelHeader>
-            <Flex align="center" gap="1rem">
-                <img src="/nr-builder-logo.svg" alt="NR Query Builder Logo" style={{ height: '48px', width: '48px' }} />
-                <XUIPanelHeading headingLevel={1}>New Relic Query Builder</XUIPanelHeading>
+  return (
+    <Provider>
+      <div className="xui-page-width-large xui-padding-large">
+        <XUIPanel header={header}>
+          <CommonQueriesPanelSection />
+
+          <XUIPanelSection className="xui-padding-large">
+            <XUIPanelSectionHeading headingLevel={2}>
+              General Query Filters
+            </XUIPanelSectionHeading>
+
+            <Flex gap="1rem" className="xui-margin-top-small">
+              <FlexItem flex={1}>
+                <ApplicationSelector />
+              </FlexItem>
+              <FlexItem flex={1}>
+                <EnvironmentSelector />
+              </FlexItem>
+              <FlexItem flex={1}>
+                <TimePeriodSelector />
+              </FlexItem>
             </Flex>
-        </XUIPanelHeader>
-    )
+          </XUIPanelSection>
 
-    return (
-        <Provider>
-            <div className="xui-page-width-large xui-padding-large">
-                <XUIPanel
-                    header={header}
-                >
-                    <CommonQueriesPanelSection />
+          <XUIPanelSection className="xui-padding-large">
+            <XUIPanelSectionHeading headingLevel={2}>
+              Metric to Query
+            </XUIPanelSectionHeading>
 
-                    <XUIPanelSection className="xui-padding-large">
-                        <XUIPanelSectionHeading headingLevel={2}>General Query Filters</XUIPanelSectionHeading>
+            <MetricQueryBuilder />
+          </XUIPanelSection>
 
-                        <Flex gap="1rem" className="xui-margin-top-small">
-                            <FlexItem flex={1}>
-                                <ApplicationSelector />
-                            </FlexItem>
-                            <FlexItem flex={1}>
-                                <EnvironmentSelector />
-                            </FlexItem>
-                            <FlexItem flex={1}>
-                                <TimePeriodSelector />
-                            </FlexItem>
-                        </Flex>
-                    </XUIPanelSection>
+          <XUIPanelSection className="xui-padding-large">
+            <XUIPanelSectionHeading headingLevel={2}>
+              View Options
+            </XUIPanelSectionHeading>
 
-                    <XUIPanelSection className="xui-padding-large">
-                        <XUIPanelSectionHeading headingLevel={2}>Metric to Query</XUIPanelSectionHeading>
+            <Flex gap="1rem" className="xui-margin-top">
+              <FlexItem flex={1}>
+                <QueryOptions />
+              </FlexItem>
+              <FlexItem flex={1}>
+                <FacetSelector />
+              </FlexItem>
+            </Flex>
+          </XUIPanelSection>
 
-                        <MetricQueryBuilder />
-                    </XUIPanelSection>
-
-                    <XUIPanelSection className="xui-padding-large">
-                        <XUIPanelSectionHeading headingLevel={2}>View Options</XUIPanelSectionHeading>
-
-                        <Flex gap="1rem" className="xui-margin-top">
-                            <FlexItem flex={1}>
-                                <QueryOptions />
-                            </FlexItem>
-                            <FlexItem flex={1}>
-                                <FacetSelector />
-                            </FlexItem>
-                        </Flex>
-                    </XUIPanelSection>
-
-
-                    <XUIPanelSection className="xui-padding-large">
-                        <XUIPanelSectionHeading headingLevel={2}>NewRelic Query</XUIPanelSectionHeading>
-                        <QueryPreview />
-                    </XUIPanelSection>
-                </XUIPanel>
-
-            </div>
-        </Provider>
-    );
+          <XUIPanelSection className="xui-padding-large">
+            <XUIPanelSectionHeading headingLevel={2}>
+              NewRelic Query
+            </XUIPanelSectionHeading>
+            <QueryPreview />
+          </XUIPanelSection>
+        </XUIPanel>
+      </div>
+    </Provider>
+  );
 }
 
 export default App;
